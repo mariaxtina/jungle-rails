8df823e0e3a9274e8c4c445b4f9eb547b0ac60a5
mariaxtina <mchristinahess@gmail.com>
2016-08-01 15:23:01 -0700

    Create password length method and class authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
+  def password_length?(password)
+    password.length > 1
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by email:email
+    !!(user && user.authenticate(password))
+  end
 end
