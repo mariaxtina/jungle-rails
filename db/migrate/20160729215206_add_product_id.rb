221b4891ca8d38ccdb1385cc12b3d1e7084eb8eb
Christina Hess <mchristinahess@gmail.com>
2016-08-02 02:26:44 +0000

    Edit seed file

--- db/migrate/20160729215206_add_product_id.rb
+++ db/migrate/20160729215206_add_product_id.rb
@@ -1,8 +1,5 @@
 class AddProductId < ActiveRecord::Migration
   def change
-     # add_reference :reviews, :product, foreign_key: true
-     # add_reference :reviews, :user, foreign_key: true
-
      add_column :reviews, :product_id, :integer
      add_column :reviews, :user_id, :integer
   end
