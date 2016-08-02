f36f582770cd4cfde45b1e597796312288dc2d98
Christina Hess <mchristinahess@gmail.com>
2016-08-02 02:38:40 +0000

    More config

--- db/migrate/20160802023036_add_users.rb
+++ db/migrate/20160802023036_add_users.rb
@@ -1,6 +1,6 @@
 class AddUsers < ActiveRecord::Migration
   def change
-    create_table :products do |t|
+    create_table :users do |t|
       t.string :name
       t.text :email
       t.string :password_digest
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801221931) do
+ActiveRecord::Schema.define(version: 20160802023036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,14 +67,13 @@ ActiveRecord::Schema.define(version: 20160801221931) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
+    t.text     "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
-  add_foreign_key "reviews", "products"
-  add_foreign_key "reviews", "users"
 end
